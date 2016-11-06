a76c6f218bc5b1db7e630150459637fa54bc6a01
Cédric Félizard <cedric@felizard.fr>
2016-11-06 11:33:26 +0000

    Update tests for customer controller

--- test/controllers/customer_controller_test.exs
+++ test/controllers/customer_controller_test.exs
@@ -2,14 +2,23 @@ defmodule Buckynix.CustomerControllerTest do
   use Buckynix.ConnCase
 
   alias Buckynix.Customer
-  @valid_attrs %{email: "joe#{Enum.random(1..100)}@test.local", name: "Joe"}
-  @invalid_attrs %{}
+  @valid_attrs %{email: "joe@test.local", name: "Joe"}
+  @invalid_attrs %{name: ""}
+
+  @account_attrs %{currency: "EUR"}
 
   setup context do
     user = Repo.insert! %Buckynix.User{name: "Name", email: "test@example.com"}
     conn = assign context[:conn], :current_user, user
 
-    [conn: conn]
+    customer = case context[:with_customer] do
+      true -> Customer.changeset(%Customer{}, @valid_attrs)
+              |> put_assoc(:account, @account_attrs)
+              |> Repo.insert!
+      _ -> nil
+    end
+
+    [conn: conn, customer: customer, customer: customer]
   end
 
   test "lists all entries on index", %{conn: conn} do
@@ -33,8 +42,8 @@ defmodule Buckynix.CustomerControllerTest do
     assert html_response(conn, 200) =~ "New customer"
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    customer = Repo.insert! Customer.changeset(%Customer{}, @valid_attrs)
+  @tag :with_customer
+  test "shows chosen resource", %{conn: conn, customer: customer} do
     conn = get conn, customer_path(conn, :show, customer)
     assert html_response(conn, 200) =~ customer.name
   end
@@ -45,27 +54,27 @@ defmodule Buckynix.CustomerControllerTest do
     end
   end
 
-  test "renders form for editing chosen resource", %{conn: conn} do
-    customer = Repo.insert! Customer.changeset(%Customer{}, @valid_attrs)
+  @tag :with_customer
+  test "renders form for editing chosen resource", %{conn: conn, customer: customer} do
     conn = get conn, customer_path(conn, :edit, customer)
     assert html_response(conn, 200) =~ "Edit customer"
   end
 
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    customer = Repo.insert! Customer.changeset(%Customer{}, @valid_attrs)
+  @tag :with_customer
+  test "updates chosen resource and redirects when data is valid", %{conn: conn, customer: customer} do
     conn = put conn, customer_path(conn, :update, customer), customer: @valid_attrs
     assert redirected_to(conn) == customer_path(conn, :show, customer)
     assert Repo.get_by(Customer, @valid_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    customer = Repo.insert! Customer.changeset(%Customer{}, @valid_attrs)
+  @tag :with_customer
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, customer: customer} do
     conn = put conn, customer_path(conn, :update, customer), customer: @invalid_attrs
     assert html_response(conn, 200) =~ "Edit customer"
   end
 
-  test "deletes chosen resource", %{conn: conn} do
-    customer = Repo.insert! Customer.changeset(%Customer{}, @valid_attrs)
+  @tag :with_customer
+  test "deletes chosen resource", %{conn: conn, customer: customer} do
     conn = delete conn, customer_path(conn, :delete, customer)
     assert redirected_to(conn) == customer_path(conn, :index)
     refute Repo.get(Customer, customer.id)
